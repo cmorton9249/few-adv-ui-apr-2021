f84f9126cc58fd309c10eb0376569257ad410d34
Christopher Morton <cmorton9249@users.noreply.github.com>
2021-04-28 12:25:59 -0400

    Created home-repairs container and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeRepairsComponent } from './containers/home-repairs/home-repairs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    HomeRepairsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home-repairs/home-repairs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-repairs',
+  templateUrl: './home-repairs.component.html',
+  styleUrls: ['./home-repairs.component.css']
+})
+export class HomeRepairsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
